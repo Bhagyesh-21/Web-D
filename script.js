97b711fe00d4320d5ae4c760b542c5718d7f0c92
BHAGYESH PATIL <bhagyesh.patil20@vit.edu>
2024-05-22 12:53:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+
+function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle("dark-mode");
+  }
+
+
+
+  
